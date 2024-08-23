0b6d64e78e1185239a41d4488ff27b329edd2dc5
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2024-08-23 10:04:16 -0300

    Tentando fazer um codigo

---
+++ src/meusComponentes/Contador.js
@@ -0,0 +1,13 @@
+
+import { Component }from "react";
+
+export default class Contador extends Component{
+
+    constructor(props){
+        super(); //executa o construtor do pai
+        this.props = props;
+        this.state = {
+                        valorAtual : 
+                     }       
+    }
+}
